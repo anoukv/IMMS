32e257f87861ef26edf8b1990e73b77bd919ebf9
anoukv <anouk.visser@student.uva.nl>
2013-11-29 14:56:38 +0100

    fixed

--- Assignment4/ransac.m
+++ Assignment4/ransac.m
@@ -4,15 +4,15 @@ function [ transformation ] = ransac(n, coordinates1, coordinates2, p)
 % coordinates1 / coordinates2 = 2xMatches coordinates
 % p - number of matches from total set of matches (t)
 
-%for i = 1:n
+for i = 1:n
     % Pick P matches at random from the total set of matches
+    
     indices = randperm(size(coordinates1, 2), p);
     data1 = coordinates1(:, indices);
     data2 = coordinates2(:, indices);
-    getAffineTransformation(data1, data2);
-    
-    
-    
+    [m1, m2, m3, m4, t1, t2] = getAffineTrasformation(data1, data2);
+end
+
 end
 
 
