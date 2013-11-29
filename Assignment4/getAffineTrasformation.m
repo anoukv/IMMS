798023987bbcb4ddf2092e5410746064774bc509
anoukv <anouk.visser@student.uva.nl>
2013-11-29 15:12:55 +0100

    pseudocode for the rest of it

--- Assignment4/getAffineTrasformation.m
+++ Assignment4/getAffineTrasformation.m
@@ -2,14 +2,17 @@ function [ m1, m2, m3, m4, t1, t2 ] = getAffineTrasformation( points1, points2 )
 
 x1 = points1(1, :);
 y1 = points1(2, :);
+x2 = points2(1, :)';
+y2 = points2(2, :)';
 
 o = ones(size(x1, 2), 1);
 z = zeros(size(x1, 2), 1);
+
 upper = [x1', y1', z, z, o, z];
 lower = [z, z, x1', y1', z, o];
 
 A = [upper;lower];
-b = reshape(points2, 20, 1);
+b = [x2; y2];
 
 transformationVector = pinv(A) * b;
 m1 = transformationVector(1);
@@ -17,7 +20,7 @@ m2 = transformationVector(2);
 m3 = transformationVector(3);
 m4 = transformationVector(4);
 t1 = transformationVector(5);
-t1 = transformationVector(6);
+t2 = transformationVector(6);
 
 end
 
--- Assignment4/ransac.m
+++ Assignment4/ransac.m
@@ -4,14 +4,23 @@ function [ transformation ] = ransac(n, coordinates1, coordinates2, p)
 % coordinates1 / coordinates2 = 2xMatches coordinates
 % p - number of matches from total set of matches (t)
 
-for i = 1:n
+mostInliers = 0;
+%for i = 1:n
     % Pick P matches at random from the total set of matches
     
     indices = randperm(size(coordinates1, 2), p);
-    data1 = coordinates1(:, indices);
-    data2 = coordinates2(:, indices);
+    data1 = coordinates1(:, indices)
+    data2 = coordinates2(:, indices)
     [m1, m2, m3, m4, t1, t2] = getAffineTrasformation(data1, data2);
-end
+    
+    % draw something somewhere (see assignment)
+    
+    % transform data1 with [m1, m2, ...]. 
+    % compare this to actual data2
+    % every transformed pixel in 10 pixel radius of original point in data2
+    % is inlier, all other outliers. If numberOfInliers > mostInliers, save
+    % the inliers (?) and [m1, m2, ...] as best model. Return [m1, m2, ..]
+%end
 
 end
 
