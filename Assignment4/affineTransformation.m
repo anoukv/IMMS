7052abbac0d1d57a905dec4f3c3902244f2e1d0a
anoukv <anouk.visser@student.uva.nl>
2013-11-29 13:46:17 +0100

    ransac function

--- Assignment4/affineTransformation.m
+++ Assignment4/affineTransformation.m
@@ -1,7 +1,12 @@
 function [ affineMatrix ] = affineTransformation( im1, im2 )
 
+% A tutorial with functions that actually exist:
+% http://www.vlfeat.org/overview/sift.html
+
 [frames1, desc1] = vl_sift(im1);
 [frames2, desc2] = vl_sift(im2);
+
+[matches] = vl_ubcmatch(desc1, desc2);
 
 
 end
---
+++ Assignment4/ransac.m
@@ -0,0 +1,9 @@
+function [ output_args ] = ransac(n, t, p)
+% INPUT
+% n - number of iterations
+% t - set of matches
+% p - number of matches from total set of matches (t)
+
+
+end
+
