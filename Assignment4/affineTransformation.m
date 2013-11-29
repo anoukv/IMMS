179dea32a085f6284974ca735e36417550106a1a
anoukv <anouk.visser@student.uva.nl>
2013-11-29 13:30:56 +0100

    sift working'

--- Assignment4/affineTransformation.m
+++ Assignment4/affineTransformation.m
@@ -1,4 +1,8 @@
 function [ affineMatrix ] = affineTransformation( im1, im2 )
 
+[frames1, desc1] = vl_sift(im1);
+[frames2, desc2] = vl_sift(im2);
+
+
 end
 
--- Assignment4/boatScript.m
+++ Assignment4/boatScript.m
@@ -1,2 +1,4 @@
 im1 = im2single(imread('img1.pgm'));
 im2 = im2single(imread('img2.pgm'));
+
+affineTransformation(im1, im2);
