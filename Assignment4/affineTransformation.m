fe614cde835e09430371a8af25f29ff531c85f7f
RemideZ <remi.de.z@gmail.com>
2013-12-02 17:43:57 +0100

    nothing

--- Assignment4/affineTransformation.m
+++ Assignment4/affineTransformation.m
@@ -1,4 +1,4 @@
-function [ M, t ] = computeAffineTransformation( im1, im2 )
+function [ bestM, bestT ] = affineTransformation( im1, im2 )
 
 % A tutorial with functions that actually exist:
 % http://www.vlfeat.org/overview/sift.html
@@ -28,7 +28,7 @@ for i = 1:size(matches, 2)
     coordinates2(2, i) = frames2(2, matches(2, i));
 end
 
-[M, t] = ransac(15, coordinates1, coordinates2, 10);
+[ bestM, bestT ] = ransac(3, coordinates1, coordinates2, 10);
 
 end
 
