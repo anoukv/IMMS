d6534b3da896c29eb22548d90620c52348e493ac
RemideZ <remi.de.z@gmail.com>
2013-12-03 17:18:58 +0100

    nothing

--- Assignment4/stitchScript.m
+++ Assignment4/stitchScript.m
@@ -1,10 +1,12 @@
 % stitch script
 left = im2single(rgb2gray(imread('left.jpg')));
 right = im2single(rgb2gray(imread('right.jpg')));
-n = 10;
-p = 10;
+n = 1000;
+p = 20;
 
-[ bestM, bestT ] = computeAffineTransformation(im1, im2, n, p);
+[ bestM, bestT ] = computeAffineTransformation(left, right, n, p);
+
+bestM = inv(bestM);
 
 T = maketform('affine', [bestM(1,1), bestM(1,2); bestM(2,1), bestM(2,2); -bestT']);
 
