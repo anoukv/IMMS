606f197e03eb88aa884d379989c627ea5d399cea
anoukv <anouk.visser@student.uva.nl>
2013-12-03 17:18:34 +0100

    GA HET MAKENgit add --all

--- Assignment4/stitchScript.m
+++ Assignment4/stitchScript.m
@@ -4,17 +4,19 @@ right = im2single(rgb2gray(imread('right.jpg')));
 n = 10;
 p = 10;
 
-[ bestM, bestT ] = computeAffineTransformation(im1, im2, n, p);
+[ M, t ] = computeAffineTransformation(left, right, n, p);
 
-T = maketform('affine', [bestM(1,1), bestM(1,2); bestM(2,1), bestM(2,2); -bestT']);
+M = inv(M);
 
+I = transformImage(left, M, t);
 
 subplot(1, 2, 1);
 imshow(left, []);
 title('left');
+
 subplot(1, 2, 2);
 imshow(right, []);
 title('right');
 
-figure, imshow(imtransform(right, T));
+figure, imshow(I);
 title('Transformed right')
