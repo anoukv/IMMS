7b2cd0830f9808414a7658b2d2567fd4507fde9e
anoukv <anouk.visser@student.uva.nl>
2013-12-03 15:47:44 +0100

    no for-loop annymore

--- Assignment4/boatScript.m
+++ Assignment4/boatScript.m
@@ -7,7 +7,7 @@ im2 = im2single(imread('img2.pgm'));
 % imshow(im2, []);
 
 [ bestM, bestT ] = computeAffineTransformation(im1, im2);
-
+size([bestM(1,1), bestM(1,2); bestM(2,1), bestM(2,2); bestT'])
 T = maketform('affine', [bestM(1,1), bestM(1,2); bestM(2,1), bestM(2,2); bestT']);
-imshow(imtransform(im2, T));
+imshow(imtransform(im1, T));
 figure, imshow(im2);
