41b2910117fd0fe5fa41d89d4f2d71f9be10146b
anoukv <anouk.visser@student.uva.nl>
2013-12-04 18:21:45 +0100

    built-in

--- Assignment4/boatScript.m
+++ Assignment4/boatScript.m
@@ -1,26 +1,27 @@
 im1 = im2single(imread('img1.pgm'));
-im2 = im2single(imread('img2.pgm'));
+im2 = im2single(imread('img6.pgm'));
 n = 10;
 p = 4;
 
-subplot(1, 2, 1);
-imshow(im1, []);
-title('im1');
-subplot(1, 2, 2);
-imshow(im2, []);
-title('im2');
+subplot(1, 3, 2);
+imshow(im1);
+title('Target');
+subplot(1, 3, 1);
+imshow(im2);
+title('Initial');
 
 [ M, t ] = computeAffineTransformation(im1, im2, n, p);
 
 % because we are going from 2 to 1
-M = inv(M);
-t = -t;
+% M = inv(M);
+% t = -t;
 
-I = transformImage(im2, M, t);
-% T = maketform('affine', [M; t']);
-% [I, XDATA, YDATA] = imtransform(im2, T);
+%I = transformImage(im2, M, t);
+T = maketform('affine', [M; t']);
+[I, XDATA, YDATA] = imtransform(im2, T);
 
-figure, imshow(I);
-title('Transformed im1');
+subplot(1, 3, 3);
+imshow(I);
+title('Transformed');
 
  
