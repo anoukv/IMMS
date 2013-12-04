560bc4592a5fdd40b0b2f9ca24252bc535e6cc95
anoukv <anouk.visser@student.uva.nl>
2013-12-04 22:07:56 +0100

    :(

--- Assignment4/boatScript.m
+++ Assignment4/boatScript.m
@@ -3,29 +3,31 @@ im2 = im2single(imread('img2.pgm'));
 n = 10;
 p = 4;
 
-% subplot(1, 3, 2);
-% imshow(im1);
-% title('Target');
-% subplot(1, 3, 1);
-% imshow(im2);
-% title('Initial');
+subplot(1, 3, 2);
+imshow(im1);
+title('Target');
+subplot(1, 3, 1);
+imshow(im2);
+title('Initial');
 
 [ M, t ] = computeAffineTransformation(im1, im2, n, p);
 
+
+T = maketform('affine', [M; t']);
+[I, XDATA, YDATA] = imtransform(im2, T);
+
+subplot(1, 3, 3);
+imshow(I);
+size(I);
+title('Transformed');
+
 % because we are going from 2 to 1
-M = inv(M);
-t = -t;
+% M = inv(M);
+% t = -t;
 
 I = transformImage(im2, M, t);
-
+figure, 
 imshow(I);
 
-
-% T = maketform('affine', [M; t']);
-% [I, XDATA, YDATA] = imtransform(im2, T);
-% 
-% subplot(1, 3, 3);
-% imshow(I);
-% title('Transformed');
 
  
--- Assignment4/transformImage.m
+++ Assignment4/transformImage.m
@@ -6,24 +6,30 @@ c2=[1;width];
 c3=[height;1];
 c4=[height; width];
     
-c1 = M * c1 + t;
-c2 = M * c2 + t;
-c3 = M * c3 + t;
-c4 = M * c4 + t;
+c1 = M * c1;
+c2 = M * c2;
+c3 = M * c3;
+c4 = M * c4;
 
-[c1,c2,c3,c4]
+% [c1,c2,c3,c4]
+% 
+% x = abs(c1(1)-c4(1));
+% x = max(x, abs(c2(1)-c3(1)));
+% y = abs(c1(2)-c4(2));
+% y = max(y, abs(c2(2)-c3(2)));
 
-x = abs(c1(1)-c4(1));
-x = max(x, abs(c2(1)-c3(1)));
-y = abs(c1(2)-c4(2));
-y = max(y, abs(c2(2)-c3(2)));
+newH = floor(max(abs(c1(1) - c4(1)), abs(c2(1) - c3(1))));
+newW = floor(max(abs(c1(2) - c4(2)), abs(c2(2) - c3(2))));
 
-transformedImage = zeros(x,y);
+transformedImage = zeros(newH,newW);
 
 [height, width] = size(transformedImage);
 
-size(transformedImage)
-size(im)
+size(transformedImage);
+size(im);
+
+M = inv(M);
+t = -t;
 
 for x = 1:height
     for y = 1:width
@@ -31,8 +37,8 @@ for x = 1:height
         p = M * p + t;
         
         if inImage(size(im), p(1), p(2))
-            p(1) = floor(p(1)+1/2);
-            p(2) = floor(p(2)+1/2);
+            p(1) = round(p(1));
+            p(2) = round(p(2));
             value = im(p(1), p(2));
         else
             value = 0;
