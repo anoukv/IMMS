f945c97fc37ea26bb8628e3709344111de40d89b
RemideZ <remi.de.z@gmail.com>
2013-12-05 22:23:58 +0100

    bus

--- Assignment4/bus.m
+++ Assignment4/bus.m
@@ -2,9 +2,6 @@
 left = im2single(rgb2gray(imread('left.jpg')));
 right = im2single(rgb2gray(imread('right.jpg')));
 
-% left = im2single(imread('img1.pgm'));
-% right = im2single(imread('img2.pgm'));
-
 % subplot(1, 2, 1);
 % imshow(left, []);
 % title('left');
@@ -13,35 +10,53 @@ right = im2single(rgb2gray(imread('right.jpg')));
 % imshow(right, []);
 % title('right');
 
-n = 10;
+n = 18;
 p = 4;
 
 % [ M, t ] = computeAffineTransformation(left, right, n, p);
-
 M = [0.9875, -0.0878 ; 0.0833, 0.9956];
 t = [-204.9172; -54.7405];
 
-T = maketform('affine', [M; t']);
-[rightNew, XDATA, YDATA] = imtransform(right, T);
+builtin=1;
+if builtin
+    
+    M = inv(M);
 
-rightNew = rightNew';
+    T = maketform('affine', [M; t']);
 
-t = abs(ceil(t));
+    [x, y] = tformfwd(T,[1 size(left,2)], [1 size(left,1)]);
 
-A = zeros(t(1), t(2));
-B = zeros(size(rightNew, 1), t(2));
-C = zeros(t(1), size(rightNew, 2));
-D = rightNew;
+    xdata = [min(1,x(1)) max(size(right,2),x(2))];
+    ydata = [min(1,y(1)) max(size(right,1),y(2))];
 
-row1 = [A;B]';
-row2 = [C;D]';
+    leftNew = imtransform(imread('left.jpg'),T,'Xdata',xdata,'YData',ydata);
+    rightNew = imtransform(imread('right.jpg'), maketform('affine', [1 0 0; 0 1 0; 0 0 1]),'Xdata',xdata,'YData',ydata); 
 
-rightTranslated = [row1;row2;];
+    imshow(max(leftNew,rightNew));
+else
+    T = maketform('affine', [M; t']);
+    [rightNew, XDATA, YDATA] = imtransform(right, T);
 
-leftResampled = zeros(size(rightTranslated));
-leftResampled(1:size(left,1), 1:size(left,2)) = left;
+    rightNew = rightNew';
+
+    t = abs(ceil(t));
+
+    A = zeros(t(1), t(2));
+    B = zeros(size(rightNew, 1), t(2));
+    C = zeros(t(1), size(rightNew, 2));
+    D = rightNew;
+
+    row1 = [A;B]';
+    row2 = [C;D]';
+
+    rightTranslated = [row2; row1];
+
+    
+    leftResampled = zeros(size(rightTranslated));
+    leftResampled(1:size(left,1), 1:size(left,2)) = left;
 
 
-figure, imshow(max(leftResampled,rightTranslated));
+    figure, imshow(max(leftResampled,rightTranslated));
+end
 
 clear();
