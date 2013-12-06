44ac3d16e5a88fd44dd55fb9321f22a1a13c11fb
anoukv <anouk.visser@student.uva.nl>
2013-12-06 13:51:12 +0100

    setup

--- IMS_Project/buildVocabulary.m
+++ IMS_Project/buildVocabulary.m
@@ -1,4 +1,9 @@
-function [  ] = buildVocabulary(  )
+function [  ] = buildVocabulary(vocabularySize, numberOfTrainingImages)
+
+% vocabularySize - this is the number of words in the vocabulary, also it
+% is the number of clusters that we are looking for with k-means.
+% numberOfTrainingImages - number of images used to build the vocabulary
+
 
 end
 
--- IMS_Project/extractFeaturesAndDescriptors.m
+++ IMS_Project/extractFeaturesAndDescriptors.m
@@ -1,4 +1,12 @@
-function [  ] = extractFeaturesAndDescriptors( )
+function [  ] = extractFeaturesAndDescriptors(im, feature)
+
+% im - the image in which we want to find features
+% feature - the kind of feature we want to detect:
+% SIFT
+% RGBSIFT
+% rgbSIFT
+% opponentSIFT
+
 
 end
 
