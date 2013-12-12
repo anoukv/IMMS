d5c6e46a825c2d772e29a5a0c6bb31ec77dd67e7
anoukv <anouk.visser@student.uva.nl>
2013-12-12 16:41:17 +0100

    hallo

--- IMS_Project/quantize.m
+++ IMS_Project/quantize.m
@@ -11,6 +11,6 @@ end
 distances = vl_alldist2(vocabulary', double(desc));
 [~, indices] = min(distances);
 
-bins = hist(indices, size(vocabulary, 1))
+bins = hist(indices, size(vocabulary, 1));
 bins = bins / size(desc, 2);
 end
