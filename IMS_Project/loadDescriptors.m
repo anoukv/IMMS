ab79221c83fbe8fc6cbc7ceec38c13ef6e291caf
RemideZ <remi.de.z@gmail.com>
2013-12-23 17:23:50 +0100

    hy

--- IMS_Project/loadDescriptors.m
+++ IMS_Project/loadDescriptors.m
@@ -3,7 +3,7 @@ function [v ] = loadDescriptors( class, number )
 name = strcat('../../IMS_data/Descriptors/', class, '/image', int2str(number));
 
 v = load(name, 'desc');
-v = v.desc;
+v = single(v.desc);
 
 end
 
