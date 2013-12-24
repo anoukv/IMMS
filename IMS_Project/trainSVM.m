b63721903d2e47a722e8660a37655ee9bfbcf2a0
anoukv <anouk.visser@student.uva.nl>
2013-12-24 12:38:24 +0100

    formating

--- IMS_Project/trainSVM.m
+++ IMS_Project/trainSVM.m
@@ -1,6 +1,9 @@
 function [ model ] = trainSVM( class )
+
 classNames = {'motorbikes_train', 'cars_train', 'faces_train', 'airplanes_train'};
+
 negativeExamples = zeros(0, 0);
+
 for i = 1:size(classNames, 2)
     if strcmp(classNames{i},class) == 0
         bins = loadBins(classNames{i});
@@ -9,11 +12,12 @@ for i = 1:size(classNames, 2)
         negativeExamples(startRow:endRow, :) = bins;
     end
 end
+
 positiveExamples = loadBins(class);
 
-groundTruth = [ones(size(positiveExamples, 1), 1);zeros(size(negativeExamples, 1), 1)]
+groundTruth = [ones(size(positiveExamples, 1), 1);zeros(size(negativeExamples, 1), 1)];
 training = [positiveExamples;negativeExamples];
-
 model = svmtrain(groundTruth, training);
+
 end
 
