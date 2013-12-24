e14cd8fdb408dd3ac33dca843a2b7102c8819437
anoukv <anouk.visser@student.uva.nl>
2013-12-24 17:04:01 +0100

    silent

--- IMS_Project/statistics.m
+++ IMS_Project/statistics.m
@@ -4,11 +4,12 @@ classifiers = getClassClassifiers();
 
 classNames = {'motorbikes_test', 'cars_test', 'faces_test', 'airplanes_test'};
 
-for i=1:size(classNames,1)
+for i=1:size(classNames,2)
+    disp(classNames{i});
     bins = loadBins(classNames{i});
     results = zeros(size(bins,1),2);
     for j=1:size(bins,1)
-        results(j,:) = predictClass(classifiers, bins(j), i)
+        results(j,:) = predictClass(classifiers, bins(j), i);
     end
 end
 
