4170c0636fa0fce10ef7e215f827e9ddb7454f88
RemideZ <remi.de.z@gmail.com>
2013-12-24 17:02:25 +0100

    hy

--- IMS_Project/statistics.m
+++ IMS_Project/statistics.m
@@ -4,14 +4,17 @@ classifiers = getClassClassifiers();
 
 classNames = {'motorbikes_test', 'cars_test', 'faces_test', 'airplanes_test'};
 
-for i=1:size(classNames,1)
+means = zeros(size(classNames,2),2);
+for i=1:size(classNames,2)
     bins = loadBins(classNames{i});
     results = zeros(size(bins,1),2);
     for j=1:size(bins,1)
-        results(j,:) = predictClass(classifiers, bins(j), i)
+        [t, p] = predictClass(classifiers, bins(j,:), i)
     end
+    means(i,:) = [mean(results(:,1)), mean(results(:,2))];
 end
 
+means
 
 end
 
