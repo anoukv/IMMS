6dcf95e4c1754c05c1acfcd99e8cde1982754e75
RemideZ <remi.de.z@gmail.com>
2013-12-25 12:12:49 +0100

    grey

--- IMS_Project/loadBins.m
+++ IMS_Project/loadBins.m
@@ -6,6 +6,7 @@ binNames = dir(fullfile(folder, '*.mat'));
 binNames = {binNames.name}';
 
 allBins = zeros(0,0);
+
 for im = 1:size(binNames,1)
     bin = load(strcat('../../IMS_data/Binned/', class, '/', binNames{im}), 'bins');
     allBins(im, :) = bin.bins;
