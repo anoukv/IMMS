9920a341a11e56d7de930d832f2ec492500a185c
RemideZ <remi.de.z@gmail.com>
2013-12-25 11:43:44 +0100

    nothing

--- IMS_Project/statistics.m
+++ IMS_Project/statistics.m
@@ -9,7 +9,7 @@ for i=1:size(classNames,2)
     bins = loadBins(classNames{i});
     results = zeros(size(bins,1),2);
     for j=1:size(bins,1)
-        [t, p] = predictClass(classifiers, bins(j,:), i)
+        [t, p] = predictClass(classifiers, bins(j,:), i);
     end
     means(i,:) = [mean(results(:,1)), mean(results(:,2))];
 end
