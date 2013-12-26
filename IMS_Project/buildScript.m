7d4ce678185a8960cc2ea90ebee0053030b2aa0e
RemideZ <remi.de.z@gmail.com>
2013-12-26 17:41:27 +0100

    uitelkaar

--- IMS_Project/buildScript.m
+++ IMS_Project/buildScript.m
@@ -17,15 +17,28 @@ end
 colorspaces = {'rgb', 'caps_rgb', 'gray', 'opp'};
 dens = [1,0];
 
+disp('Extracting descriptors');
 for i=1:size(colorspaces,2)
-    disp(strcat(32,32));
     disp(strcat('Started on color:',32, colorspaces{i}));
     for d=1:size(dens,2)
         buildDescriptors(sizeOfRun,'train', dens(d), colorspaces{i});
         buildDescriptors(testSize,'test', dens(d), colorspaces{i});
 
-        buildVocabulary(vocSizes, vocNimages, dens(d), colorspaces{i});
+    end
+end
 
+disp('Building vocabularies');
+for i=1:size(colorspaces,2)
+    disp(strcat('Started on color:',32, colorspaces{i}));
+    for d=1:size(dens,2)
+        buildVocabulary(vocSizes, vocNimages, dens(d), colorspaces{i});
+    end
+end
+
+disp('Building bins');
+for i=1:size(colorspaces,2)
+    disp(strcat('Started on color:',32, colorspaces{i}));
+    for d=1:size(dens,2)
         for j=1:size(vocSizes,1)
             v = loadVocabulary(vocSizes(j), vocNimages, dens(d), colorspaces{i});
             buildBins(1, sizeOfRun, v, 'train',  dens(d), colorspaces{i});
