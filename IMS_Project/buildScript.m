9898748e40f1d2d471b7bc19717f12e254b069c8
RemideZ <remi.de.z@gmail.com>
2013-12-26 19:57:44 +0100

    nicerer

--- IMS_Project/buildScript.m
+++ IMS_Project/buildScript.m
@@ -1,4 +1,4 @@
-function [] = buildScript( sizeOfRun, vocSizes, vocNimages )
+function [] = buildScript( sizeOfRun, vocSizes, vocNimages, doDescriptors, doVocabulary, doBins )
 
 if nargin == 0
     sizeOfRun = 100;
@@ -17,32 +17,38 @@ end
 colorspaces = {'rgb', 'caps_rgb', 'gray', 'opp'};
 dens = [1,0];
 
-disp('Extracting descriptors');
-for i=1:size(colorspaces,2)
-    disp(strcat('Started on color:',32, colorspaces{i}));
-    for d=1:size(dens,2)
-        buildDescriptors(sizeOfRun,'train', dens(d), colorspaces{i});
-        buildDescriptors(testSize,'test', dens(d), colorspaces{i});
+if doDescriptors
+    disp('Extracting descriptors');
+    for i=1:size(colorspaces,2)
+        disp(strcat('Started on color:',32, colorspaces{i}));
+        for d=1:size(dens,2)
+            buildDescriptors(sizeOfRun,'train', dens(d), colorspaces{i});
+            buildDescriptors(testSize,'test', dens(d), colorspaces{i});
 
+        end
     end
 end
 
-disp('Building vocabularies');
-for i=1:size(colorspaces,2)
-    disp(strcat('Started on color:',32, colorspaces{i}));
-    for d=1:size(dens,2)
-        buildVocabulary(vocSizes, vocNimages, dens(d), colorspaces{i});
+if doVocabulary
+    disp('Building vocabularies');
+    for i=1:size(colorspaces,2)
+        disp(strcat('Started on color:',32, colorspaces{i}));
+        for d=1:size(dens,2)
+            buildVocabulary(vocSizes, vocNimages, dens(d), colorspaces{i});
+        end
     end
 end
 
-disp('Building bins');
-for i=1:size(colorspaces,2)
-    disp(strcat('Started on color:',32, colorspaces{i}));
-    for d=1:size(dens,2)
-        for j=1:size(vocSizes,1)
-            v = loadVocabulary(vocSizes(j), vocNimages, dens(d), colorspaces{i});
-            buildBins(1, sizeOfRun, v, 'train',  dens(d), colorspaces{i});
-            buildBins(1, testSize, v, 'test',  dens(d), colorspaces{i});
+if doBins
+    disp('Building bins');
+    for i=1:size(colorspaces,2)
+        disp(strcat('Started on color:',32, colorspaces{i}));
+        for d=1:size(dens,2)
+            for j=1:size(vocSizes,1)
+                v = loadVocabulary(vocSizes(j), vocNimages, dens(d), colorspaces{i});
+                buildBins(1, sizeOfRun, v, 'train',  dens(d), colorspaces{i});
+                buildBins(1, testSize, v, 'test',  dens(d), colorspaces{i});
+            end
         end
     end
 end
--- IMS_Project/buildVocabulary.m
+++ IMS_Project/buildVocabulary.m
@@ -9,8 +9,13 @@ clusterSizes = clusterSizes';
 for i = 1:size(clusterSizes,1)
     warning('off','all');
     % clusters = vl_kmeans(allDescriptors, clusterSizes(i));
-%     [~, clusters] = kmeans(allDescriptors, clusterSizes(i), 'start', 'cluster', 'options', statset('MaxIter', 20));
-    [~, clusters] = kmeans(allDescriptors, clusterSizes(i), 'options', statset('MaxIter', 100));
+    % [~, clusters] = kmeans(allDescriptors, clusterSizes(i), 'options', statset('MaxIter', 100));
+    try
+        [~, clusters] = kmeans(allDescriptors, clusterSizes(i), 'start', 'cluster', 'options', statset('MaxIter', 20));
+    catch
+        disp('Catching empty cluster error...');
+        buildVocabulary(clusterSizes(i), numberOfImagesPerClass, dens, colorspace)
+    
     warning('on','all');
     save(strcat('../../IMS_data/Vocabulary_', int2str(clusterSizes(i)), 'x', int2str(numberOfImagesPerClass), suffix), 'clusters');
 end
