3210d09b6abab3a9ffd9043b7a2bca1c3b9ba62f
anoukv <anouk.visser@student.uva.nl>
2013-12-26 18:13:16 +0100

    ?

--- IMS_Project/loadBins.m
+++ IMS_Project/loadBins.m
@@ -2,15 +2,7 @@ function [ allBins ] = loadBins( class, dens, colorspace)
 
 folder = strcat('../../IMS_data/Binned/', class);
 suffix = strcat('_', int2str(dens), '_', colorspace);
-
-
-<<<<<<< HEAD
-suffix = strcat('_', int2str(dens), '_', colorspace);
-
-binNames = dir(fullfile(folder, strcat('*', suffix, '.mat'));
-=======
 binNames = dir(fullfile(folder, strcat('*', suffix, '.mat')));
->>>>>>> 769747920bb7bc053d02aaa0f693acedaa164cf5
 binNames = {binNames.name}';
 
 allBins = zeros(0,0);
