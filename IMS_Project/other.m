13a0581cd74ed863735ec0500f2ed7185b0b5c58
anoukv <anouk.visser@student.uva.nl>
2013-12-26 15:03:06 +0100

    chaosMatrix and ordered matrices

--- IMS_Project/other.m
+++ IMS_Project/other.m
@@ -5,9 +5,11 @@ classNames = {'motorbikes_test', 'cars_test', 'faces_test', 'airplanes_test'};
 for i = 1:size(classNames, 2)
     [test, truth] = generateTestData(classNames{i});
     [prediction, acc, probability] = svmpredict(truth, test, classifiers(i));
+    resultMatrix = [prediction, truth, probability];
+    sortedOnProb = sortrows(resultMatrix, -3);
+end
+
 end
 
 
-
-end
 
