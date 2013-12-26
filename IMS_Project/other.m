98deeff77d59977c6d2c3fb1d04f669261e5c042
anoukv <anouk.visser@student.uva.nl>
2013-12-26 14:19:18 +0100

    other method

---
+++ IMS_Project/other.m
@@ -0,0 +1,14 @@
+function [  ] = other( )
+classifiers = getClassClassifiers();
+classNames = {'motorbikes_test', 'cars_test', 'faces_test', 'airplanes_test'};
+
+for i = 1:size(classNames, 2)
+    [test, truth] = generateTestData(classNames{i});
+    [prediction, acc, probability] = svmpredict(truth, test, classifiers(i));
+    sum(prediction)
+end
+
+
+
+end
+
--- IMS_Project/simpleRunScript.m
+++ IMS_Project/simpleRunScript.m
@@ -7,15 +7,15 @@ if nargin == 1
     vocSize = 10;
 end
 
-buildDescriptors(1,sizeOfRun,'train');
-buildDescriptors(1,50,'test');
+%buildDescriptors(1,sizeOfRun,'train');
+%buildDescriptors(1,50,'test');
 
-buildVocabulary([vocSize],sizeOfRun);
+%buildVocabulary([vocSize],sizeOfRun);
 
 v = loadVocabulary(vocSize,sizeOfRun);
 
-buildBins(1, sizeOfRun, v, 'train');
-buildBins(1, sizeOfRun, v, 'test');
+%buildBins(1, sizeOfRun, v, 'train');
+%buildBins(1, sizeOfRun, v, 'test');
 
 statistics();
 
